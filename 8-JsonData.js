66a9f64e913f7c391242ff89ef548eed222194d4
WilldiazRaM <wi.diaz@duocuc.cl>
2024-04-21 21:40:04 -0400

    Se agrega notacion de punto y corchetes; funciona ok

--- 8-JsonData.js
+++ 8-JsonData.js
@@ -2,4 +2,15 @@ const jsonData = '{"book": {"name": "json", "price": 29.99, "inStock": true, "ra
 
 const jsonObject = JSON.parse(jsonData);
 
-console.log(jsonObject);
+//console.log(jsonObject);
+
+
+//using dot notation
+const book = jsonObject.book;
+console.log(book);
+console.log(book.name, book.price, book.inStock, book.rating);
+
+//using the bracket notation
+const book2 = jsonObject['book'];
+console.log(book2);
+console.log(book["name"], book["price"], book["inStock"]);
